716b98705d571b2fe2c7c6aa32003596d24f7947
didiha <hcring@naver.com>
2023-02-08 02:14:24 +0900

    20230208

---
+++ chap_02/_01_Operator1.java
@@ -0,0 +1,13 @@
+package chap_02;
+
+public class _01_Operator1 {
+    public static void main(String[] args) {
+        // 산술 연산자
+
+        // 일반 연산
+        System.out.println(4 + 2); // 6
+        System.out.println(4 - 2); // 2
+        System.out.println(4 * 2); // 8
+        System.out.println(4 / 2); // 2
+    }
+}
