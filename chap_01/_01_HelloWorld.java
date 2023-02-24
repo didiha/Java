6b47d3e1c9f12bf35d0c85091693db38bf0e85d6
didiha <hcring@naver.com>
2023-02-24 22:38:33 +0900

    20230224

--- chap_01/_01_HelloWorld.java
+++ chap_01/_01_HelloWorld.java
@@ -3,6 +3,6 @@ package chap_01;
 public class _01_HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("잔디 wwwwwwwwwwwww");
+        System.out.println("잔디 wwwwwwwwwwwwww");
     }
 }
