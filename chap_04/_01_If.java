f045a745f7e399f44d820f26d69af24461455835
didiha <hcring@naver.com>
2023-03-02 01:53:21 +0900

    20230302

--- chap_04/_01_If.java
+++ chap_04/_01_If.java
@@ -3,6 +3,17 @@ package chap_04;
 public class _01_If {
     public static void main(String[] args) {
         // 조건문 if
+        int hour  = 15; // 오후 3시
 
+        // if 문 내에서 한 문장만 쓸 때는 {} 생략 가능
+        if (hour >= 14)
+            System.out.println("몬스터 에너지 드링크 +1");
+
+        // 여러 문장 쓸 때는 {} 필수!
+        if (hour < 14) {
+            System.out.println("커피우유 + 1");
+            System.out.println("초코바 추가");
+        }
+        System.out.println("음료 주문 완료");
     }
 }
