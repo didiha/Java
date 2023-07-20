50ff129711511484ff594efe58c1bf8faf7b0d1f
didiha <hcring@naver.com>
2023-07-20 16:43:40 +0900

    20230720

---
+++ Baekjoon/No1001.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+import java.util.Scanner;
+
+public class No1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        int A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.println(A-B);
+    }
+}
