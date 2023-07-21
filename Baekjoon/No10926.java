6f1a4659e5c5b2f46b15932c6468a161f712e71b
didiha <hcring@naver.com>
2023-07-21 15:54:56 +0900

    20230721

---
+++ Baekjoon/No10926.java
@@ -0,0 +1,9 @@
+package Baekjoon;
+import java.util.Scanner;
+public class No10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ID = sc.nextLine();
+        System.out.println(ID + "??!");
+    }
+}
