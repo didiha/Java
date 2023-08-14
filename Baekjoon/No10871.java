cbe63bbce4b9d1088de5a5d1aaf0c62bf8081fe7
didiha <hcring@naver.com>
2023-08-14 21:30:11 +0900

    20230814

--- Baekjoon/No10871.java
+++ Baekjoon/No10871.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class No10871 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
     }
 }
