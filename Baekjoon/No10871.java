0804af67d79714c51db88c59616fc3f6e04e4ecd
didiha <hcring@naver.com>
2023-08-15 16:04:49 +0900

    20230815

--- Baekjoon/No10871.java
+++ Baekjoon/No10871.java
@@ -1,9 +1,27 @@
 package Baekjoon;
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 public class No10871 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        System.out.println(x);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        int N = Integer.parseInt(str.split(" ")[0]);
+        int X = Integer.parseInt(str.split(" ")[1]);
+        int[] A = new int[N];
+
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+            if(A[i] < X)
+                bw.write(A[i] + " ");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
