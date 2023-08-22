c97356406f781772dc05223fbe80641fa99bf5ab
didiha <hcring@naver.com>
2023-08-22 16:36:43 +0900

    20230822

--- Baekjoon/No2562.java
+++ Baekjoon/No2562.java
@@ -1,8 +1,31 @@
 package Baekjoon;
-
+import java.io.*;
 public class No2562 {
-    public static void main(String[] args) {
-        int max = 0;
-        System.out.println(max);
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int max;
+        int maxNumber = 1;
+        int[] array = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        max = array[0];
+
+        for (int i = 0; i < 9; i++) {
+            if(max < array[i]){
+                max = array[i];
+                maxNumber = i + 1;
+            }
+        }
+
+        bw.write(max + "\n" + maxNumber);
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
