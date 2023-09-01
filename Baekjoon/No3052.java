6fc63ca4cda3e458ee0f8ec8974fd6585543131d
didiha <hcring@naver.com>
2023-09-01 21:44:45 +0900

    20230901

--- Baekjoon/No3052.java
+++ Baekjoon/No3052.java
@@ -2,6 +2,8 @@ package Baekjoon;
 
 public class No3052 {
     public static void main(String[] args) {
-
+        System.out.println("Hey siri.");
+        int x = 9;
+        System.out.println(x);
     }
 }
