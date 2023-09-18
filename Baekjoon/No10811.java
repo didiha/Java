1602ed771df88e029190cebb4a81baaeef031a52
didiha <hcring@naver.com>
2023-09-19 03:45:18 +0900

    20230919

--- Baekjoon/No10811.java
+++ Baekjoon/No10811.java
@@ -1,7 +1,23 @@
 package Baekjoon;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class No10811 {
-    public static void main(String[] args) {
-        System.out.println("www");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int array[] = new int[N];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            array[i] = i + 1;
+        }
+
     }
 }
