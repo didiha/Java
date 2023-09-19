5f0d164a7f777955136156254215fb815de3192d
didiha <hcring@naver.com>
2023-09-19 17:51:36 +0900

    20230919

--- Baekjoon/No10811.java
+++ Baekjoon/No10811.java
@@ -2,7 +2,6 @@ package Baekjoon;
 
 import java.io.*;
 import java.util.StringTokenizer;
-
 public class No10811 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,12 +11,36 @@ public class No10811 {
         st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int array[] = new int[N];
-        int count = 0;
+        int[] array = new int[N];
+        int change1, change2;
+        
 
         for (int i = 0; i < N; i++) {
             array[i] = i + 1;
         }
 
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            change1 = Integer.parseInt(st.nextToken());
+            change2 = Integer.parseInt(st.nextToken());
+
+            int[] tmp = new int[change2 - change1 + 1];
+            
+            for (int j = change2, k = 0; j > (change1 - 1); j--, k++) {
+                tmp[k] = array[j - 1];
+            }
+
+            for (int j = (change1 - 1), k = 0; j < change2; j++, k++) {
+                array[j] = tmp[k];
+            }
+        }
+
+        for (int j = 0; j < N; j++) {
+            bw.write(array[j] + " ");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
