084681f1e2577427832845d5c80368c5405a938c
didiha <hcring@naver.com>
2023-09-22 16:47:46 +0900

    20230922

--- Baekjoon/No1546.java
+++ Baekjoon/No1546.java
@@ -1,6 +1,38 @@
 package Baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class No1546 {
-    public static void main(String[] args) {
-        System.out.println("d");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] array = new int[N];
+        int M = array[0];
+        int sum = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+            if(M < array[i])
+                M = array[i];
+        }
+        bw.write(M + "\n");
+        for (int i = 0; i < N; i++) {
+            bw.write(array[i] + " ");
+            bw.write(array[i] / M + " ");
+            bw.write(array[i] / M * 100 + " ");
+            array[i] = (array[i] / M) * 100;
+            bw.write(array[i] + "\n");
+            sum += array[i];
+        }
+        bw.write((float)sum / N + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
