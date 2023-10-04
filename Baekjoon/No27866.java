20407f676ba5d40370bcc8032988136bea119800
didiha <hcring@naver.com>
2023-10-04 21:12:47 +0900

    20231004

---
+++ Baekjoon/No27866.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class No27866 {
+    public static void main(String[] args) {
+        char S;
+        int n;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
