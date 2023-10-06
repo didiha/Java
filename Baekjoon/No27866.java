838a6cdc617bcd3d274a6940f58d522f5eb76b66
didiha <hcring@naver.com>
2023-10-06 16:00:29 +0900

    20231006

--- Baekjoon/No27866.java
+++ Baekjoon/No27866.java
@@ -1,17 +1,17 @@
 package Baekjoon;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.io.*;
 public class No27866 {
-    public static void main(String[] args) {
-        char S;
+    public static void main(String[] args) throws IOException {
+        String S;
         int n;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        S = br.readLine();
+        n = Integer.parseInt(br.readLine());
+
+        bw.write(S.charAt(n - 1));
 
         br.close();
         bw.flush();
