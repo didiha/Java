54ba7e3e9b0f2ecdfcca190ebb2167ce95921373
didiha <hcring@naver.com>
2023-10-13 18:18:34 +0900

    20231013

---
+++ Baekjoon/No11654.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.io.*;
+import java.io.IOException;
+public class No11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        
+    }
+}
