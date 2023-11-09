65032f92e38e868e592e7759e9f7e1ba65363104
didiha <hcring@naver.com>
2023-11-09 21:27:45 +0900

    20231109

---
+++ Baekjoon/No10809.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.io.*;
+
+public class No10809 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] array = new int[26];
+        String S = br.readLine();
+
+        for (int i = 0; i < 26; i++)
+            array[i] = 0;
+
+        for (int i = 0; i < S.length(); i++) {
+
+        }
+    }
+}
