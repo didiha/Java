ac677ad0c22079df855921a3823ecdcd86cc9105
didiha <hcring@naver.com>
2025-02-17 23:08:55 +0900

    rollback

---
+++ test.java
@@ -0,0 +1,14 @@
+/*public class test {
+    public class operand {
+        static int x = 10;
+
+        public static int func() {
+            x = 20;
+            return 30;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(x+func());
+    }
+}
+ */
